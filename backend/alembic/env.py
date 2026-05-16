5b69fb89930b7d817ff2d4bb5373f149f8b4e11d
Dinmukhamad <pakhriddinov.dinmukhamad@gmail.com>
2026-05-16 18:53:22 +0500

    Fix indentation in run_migrations_online function

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -35,7 +35,7 @@ def run_migrations_offline() -> None:
 def run_migrations_online() -> None:
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
+        run_migrations_online()
         poolclass=pool.NullPool,
     )
     with connectable.connect() as connection:
